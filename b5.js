4a94b4d7df9b4f55a38fea4dc930237f0f40b15c
KhacPhong-dev <kaito0676@gmail.com>
2025-03-04 15:59:45 +0700

    Update b5.js

--- b5.js
+++ b5.js
@@ -2,9 +2,9 @@ let a = +prompt("Nhập vào số a: ");
 let b = +prompt("Nhập vào số b: ");
 let result = 1;
 if (isNaN(a) || isNaN(b)) {
-    console.log("Invalid input. a and b must be numbers.");
+    console.log("Invalid input. a and b phải là số.");
 }else if (a < 1 || b < 1) {
-    console.log("Invalid input. a and b must be greater than or equal to 1."); 
+    console.log("Invalid input. a and b lớn hơn 1."); 
 }else {
     for (let i = 1; i <= b; i++) {
         result *= a;
