29b0e58732421e7e283c3b9e330243bf965f0b6b
KhacPhong-dev <kaito0676@gmail.com>
2025-03-04 15:58:27 +0700

    Update b2.js

--- b2.js
+++ b2.js
@@ -1,10 +1,10 @@
 let n = +prompt("Nhập vào một số nguyên dương: ");
 let result = "";
 if (n < 1) {
-    result = "Invalid input. N must be greater than or equal to 1.";
+    result = "Invalid input. N lớn hơn 1.";
 
 }else if (isNaN(n)) {
-    result = "Invalid input. N must be a number."; 
+    result = "Invalid input. N phải là số."; 
 }else {
     for (let i = 1; i <= n; i++) {
         if (i % 5 == 0) {
