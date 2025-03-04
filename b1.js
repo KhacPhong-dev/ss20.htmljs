7091a4f5e6f75c42de1ea5d3c2bd79d8b236f62e
KhacPhong-dev <kaito0676@gmail.com>
2025-03-04 15:57:36 +0700

    Update b1.js

--- b1.js
+++ b1.js
@@ -1,10 +1,10 @@
- function sumOfNumbers(N) {
+ function sumofNumbers(N) {
     let sum = 0;
     if (N < 1 ) {
-        console.log("Invalid input. N must be greater than or equal to 1.");
+        console.log("Invalid input. N phải lớn hơn 1.");
         return false; 
     }else if (isNaN(N)) {
-        console.log("Invalid input. N must be a number.");
+        console.log("Invalid input. N phải là số.");
         return false; 
     }
     for (let i = 1; i <= N; i++) {
@@ -15,9 +15,9 @@
 }
 
 let n = +prompt("Enter a number: ");
-let result = sumOfNumbers(n);
+let result = sumofNumbers(n);
 if (result !== false) {
-    console.log("The sum of the first " + n + " natural numbers is: " + result); 
+    console.log(result); 
 }else if (result === false){
     console.log(result); 
 }
